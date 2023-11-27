4fa52ae121c35abbe5a27b416e0afc2cbba024f6
Yusuf Abdulmuiz (Dev.Abu Ridwan) <yusufabdulmuizolasunkanmi@gmail.com>
2023-11-27 12:29:29 +0100

    Update index.js

--- public/script/index.js
+++ public/script/index.js
@@ -32,7 +32,7 @@ menuBtn.addEventListener('click', ()=>{
 
 window.onload = function (){
 
-  cookie.style.left = 0;
+  cookie.style.top = 0;
 
 }
 
@@ -40,7 +40,7 @@ cookieBtn.forEach(btn=>{
 
   btn.addEventListener('click', function () {
 
-      cookie.style.left = '-120%';
+      cookie.style.top = '-120%';
 
   })
 
